58635e1e03283a53f19c65c45172342785829d40
sonu samrat <sonusamrat@192.168.1.21>
2026-01-01 23:39:04 +0530

    first commit

---
+++ string_capitalize.py
@@ -0,0 +1,5 @@
+str = "i am a coder coder "
+print(str.count('coder'))
+print(str.index("coder"))
+print(str.isdigit())
+print(str.format())
---
+++ string_replace.py
@@ -0,0 +1,6 @@
+str = "i am studying python from apna college"
+print(str.replace("o","a"))
+
+
+str = "i am studying python from apnacollege" 
+print(str.replace("python","javascript"))
